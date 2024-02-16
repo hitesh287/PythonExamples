8fa3d01f24384a61b11d096719f5211cc130b6fc
hitesh287 <hitesh287@gmail.com>
2024-02-16 16:50:57 -0500

    Some more updates

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,9 +3,9 @@
 #Added extra comments
 
 =======
-#This is a hello world program
-#Updated comment
-Some more changes
+# This is a hello world program
+# Updated comment
+# Some more changes
 
 >>>>>>> 5d36b1ab1628fd259b84077570fe348a09da8ee2
 print ("Hello world!")
